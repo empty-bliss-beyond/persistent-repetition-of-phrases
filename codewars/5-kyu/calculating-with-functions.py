947c9ef0d1f0d6f90d9284d631c9f7e0cc95618a
empty-bliss-beyond <329003714+empty-bliss-beyond@users.noreply.github.com>
2026-09-25 02:50:57 -0300

    More...

---
+++ codewars/5-kyu/calculating-with-functions.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+# Weird stuff
+
+def zero(f = None): return f(0) if f else 0
+def one(f = None): return f(1) if f else 1
+def two(f = None): return f(2) if f else 2
+def three(f = None): return f(3) if f else 3
+def four(f = None): return f(4) if f else 4
+def five(f = None): return f(5) if f else 5
+def six(f = None): return f(6) if f else 6
+def seven(f = None): return f(7) if f else 7
+def eight(f = None): return f(8) if f else 8
+def nine(f = None): return f(9) if f else 9
+
+def plus(b: int): return lambda a: a + b
+def minus(b: int): return lambda a: a - b
+def times(b: int): return lambda a: a * b
+def divided_by(b: int): return lambda a: a // b
